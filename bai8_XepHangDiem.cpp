13f1e2a8792be7cf545b9d8d0e6154414eb26a4b
SPC <SPC@26-LAB402>
2024-12-30 16:25:11 +0700

    Add project files.

---
+++ bai8_XepHangDiem.cpp
@@ -0,0 +1,41 @@
+// bai8_XepHangDiem.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Xep Hang Diem Hoc Tap!\n";
+	// dau vao la : diem
+	// dau ra la : xep hang
+
+	double diem;
+		cout << "nhap diem "; cin >> diem;
+	if (diem < 5) {
+		cout << "yeu";
+	}
+	else if (diem <= 6) {
+		cout << "TB";
+	}
+	else if (diem < 8)
+	{
+		cout << "kha";
+	}
+	else
+	{
+		cout << "gioi";
+	}
+	
+	
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
